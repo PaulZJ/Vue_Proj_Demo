bc768e67cfb960939b4a501f41c6c203d2c954bc
PaulZJ <zhangjun8896@126.com>
2018-05-08 20:58:22 +0800

    Fix: fix ui problem with common.scss

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+import App from '../App'
 import PaulZJ from '@/components/zj'
+import login from '@/components/login/login'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'PaulZJ',
-      component: PaulZJ
+      component: App, //顶层路由，对应index.html
+      children: [
+        {
+          path: '',
+          redirect: '/home'
+        },
+        {
+          path: '/home',
+          component: PaulZJ
+        },
+        {
+          path: '/login',
+          component: login
+        }
+      ]
     }
   ]
 })
